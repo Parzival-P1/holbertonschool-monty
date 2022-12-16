b809d7833922d446b2fcfb9fd5247166c94c9e35
Paco Hdez <francj.hdez@gmail.com>
2022-12-15 20:24:08 -0600

    updating header file

--- monty.h
+++ monty.h
@@ -1,6 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer

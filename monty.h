97397e93ebc2898b16317a57caa51f02a5c4672b
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 15:34:03 -0600

    adding header file

--- monty.h
+++ monty.h
@@ -9,6 +9,23 @@
 #include <ctype.h>
 
 /**
+ * struct var_s - struct to contain the main variables of the Monty interpreter
+ * @queue: flag to determine if in stack vs queue mode
+ * @stack_len: length of the stack
+ */
+typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+#define STACK 0
+#define QUEUE 1
+
+/* global struct to hold flag for queue and stack length */
+extern var_t var;
+
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -37,6 +54,20 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* FUNCTIONS */
+
+static int check_digit(char *arg);
+void f_push(stack_t **stack, unsigned int line_number);
+void f_pop(stack_t **stack, unsigned int line_num);
+void f_pall(stack_t **stack, unsigned int line_num);
+int main(int argc, char *argv[]);
+
+
+
+
+
+
 
 #endif /* MONTY_H */
 

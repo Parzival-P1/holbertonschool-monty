2da837be93fa5911d16bb43a31aaa69beb34e9f2
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 20:53:44 -0600

    fixed

--- subs_func.c
+++ subs_func.c
@@ -17,7 +17,7 @@ void f_sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	n = (*stack)->n;
-	m_pop(stack, line_number);
+	f_pop(stack, line_number);
 	(*stack)->n -= n;
 }
 

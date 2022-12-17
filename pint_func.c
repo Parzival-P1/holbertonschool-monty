7750c6fb8be44dcfe610459bc099c4c2fe3e4538
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 18:14:38 -0600

    adding pint func

--- pint_func.c
+++ pint_func.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
  * f_pint - print the val on top of the stack, or exit if the stack is empty
- * @stack: double ptr to head of stack
+ * @stack_t: double ptr to head of stack
  * @line_num: line number of current operation
  * Return: void
  */
-void f_pint(stack, unsigned int line_num)
+void f_pint(stack_t, unsigned int line_num)
 {
 	stack_t *head = *stack;
 

059ad9a116f92599d151209653121bc9e954d6a6
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 21:50:24 -0600

    updating main.h

--- monty.h
+++ monty.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
  * struct var_s - struct to contain the main variables of the Monty interpreter

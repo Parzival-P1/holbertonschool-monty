162238ad0a69addba81e03a1d8095f834fe63001
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 20:18:17 -0600

    func to set the FIFO

---
+++ queue_func.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_queue - sets the format of the data to a queue (FIFO)
+ * @stack: dbl ptr to the beginnning of the stack
+ * @line_number: script line number
+ * Return: void
+ */
+void f_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	var.queue = QUEUE;
+}
+

8b09e6efd9da61331bab1914d71276af6e64f2c8
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 20:21:41 -0600

    func that sets the format of the LIFO

---
+++ stack_func.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_stack - sets the formar of the data to a LIFO
+ * @stack: dbl ptr to the beginning of the stack
+ * @line_number: script line num
+ * Return: void
+ */
+void f_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	var.queue = STACK;
+}
+

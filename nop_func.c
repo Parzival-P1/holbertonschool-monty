7e63de223446867a3092a78316e9776b5c809d25
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 18:59:45 -0600

    addinng nop func

---
+++ nop_func.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * f_pop - no operation performed
+ * @stack: double ptr to the head of the stack
+ * @line_number: line numebr of current operation
+ * Return: void
+ */
+void f_pop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+

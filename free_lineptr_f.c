b5de0e27a2eb9ec0ed3ac92544f1f1438477bda8
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 19:06:38 -0600

    adding freelineptr func

---
+++ free_lineptr_f.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * free_line_ptr - free the line ptr returned by getline
+ * @status: exit status
+ * @arg: ptr to the line
+ * Return: void
+ */
+void free_line_ptr(int status, void *arg)
+{
+	char **lineptr = arg;
+
+	(void)status;
+	if (*linptr != NULL)
+		free(*lineptr);
+}
+

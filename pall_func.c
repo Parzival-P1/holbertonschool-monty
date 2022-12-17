c3ad8b5c2adb89c037f8a9dbefbc558956e62902
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 18:17:06 -0600

    updating pall func

--- pall_func.c
+++ pall_func.c
@@ -2,10 +2,10 @@
 /**
  * f_pall - print all the values on the stack stsrting from the top
  * @stack: dohble ptr to the head of the stack
- * @line_num: line number being executed from script file
+ * @line_number: line number being executed from script file
  * Return: void
  */
-void f_pall(stack_t **stack, unsigned int line_num)
+void f_pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head;
 

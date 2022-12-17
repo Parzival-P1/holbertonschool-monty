8d40ce52ac52c7e4ae19ed0c0474f58b9e7aa339
Paco Hdez <francj.hdez@gmail.com>
2022-12-16 20:04:55 -0600

    adding multiply func

---
+++ f_mult.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * f_mul - multiply top two elements of stack & push the result
+ * @stack: dbl ptr to the head of the stack
+ * @line_number: line number of current operation
+ * Return: void
+ */
+void f_mul(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (var.stack_len < 2)
+	{
+		dprintf(STDOUT_FILENO
+			"L%u: can't mul, stack  too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = (*stack)->n;
+	f_pop(stack, line_number);
+	(*stack)->n *= n;
+}
+
